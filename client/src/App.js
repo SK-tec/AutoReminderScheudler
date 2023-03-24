6bd13cc2531493ba8ee58028b92ab71bec98fae5
Ershia <ershia94@gmail.com>
2023-03-24 15:27:24 +0100

    first step achivements

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Footer from './components/Footer';
+import Achivements from './components/Achivements';
 
 function App() {
   return (
     <div className="App">
+      <Achivements/>
      <Footer/>
     </div>
   );
