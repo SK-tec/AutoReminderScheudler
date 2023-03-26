1eb49b793f0b5cab7e4703166e0be06a386782cd
Tejasvi-chougule <120389379+Tejasvi-chougule@users.noreply.github.com>
2023-03-27 01:13:05 +0200

    carousal

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Footer from './components/Footer';
 import Achivements from './components/Achivements';
 import MyNav from './components/MyNav';
+import Header from './components/Header';
 import RegisterForm from './components/RegisterForm';
 
 function App() {
   return (
     <div className="App">
       <MyNav />
+      <Header />
       <Achivements/>
       <RegisterForm/>
      <Footer/>
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,7 +1,51 @@
+import Carousel from 'react-bootstrap/Carousel';
+import school from "../images/school.jpg";
+import students from "../images/students.png";
+
+
 const Header = () => {
     return ( 
     <>
-    
+    <Carousel id="carousal">
+      <Carousel.Item >
+        <img
+          className="d-block w-100"
+          src={school}
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>21 Years of Excellence</h3>
+          
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={students}
+          alt="Second slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="holder.js/800x400?text=Third slide&bg=20232a"
+          alt="Third slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+ 
     </> 
     );
 }
