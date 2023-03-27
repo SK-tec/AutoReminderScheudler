9f7845a2b0e99a21b3b15dd462d25dfb6c961c74
SK-tec <samathagamidi@gmail.com>
2023-03-27 18:12:36 +0200

    Navbar routing added and style changed

--- client/src/App.js
+++ client/src/App.js
@@ -6,16 +6,22 @@ import MyNav from './components/MyNav';
 import Header from './components/Header';
 import RegisterForm from './components/RegisterForm';
 import Signin from './components/Signin';
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <MyNav />
-      <Header />
-      <RegisterForm/>
-     <Signin />
-      <Achivements/>
-     <Footer/>
+    <div className="App">  
+         
+     <MyNav />
+      <Routes>
+        <Route path="/" element={<Header />} />
+        {/* <Route path="/aboutus" element={<AboutUs />} />
+        <Route path="/contact" element={<Contact />} /> */}
+        <Route path="/admin" element={<RegisterForm />} />
+        
+        <Route path="/signin" element={<Signin/>} /> 
+      </Routes>
+      <Footer />
     </div>
   );
 }
--- client/src/components/MyNav.js
+++ client/src/components/MyNav.js
@@ -3,15 +3,13 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { LinkContainer } from "react-router-bootstrap";
-import NavLink from 'react-router-dom'
-
-
+import NavLink from 'react-router-dom';
 
 const MyNav = () => {
     return ( 
     <>
     
-      <Navbar className="Navbar" varient="light" >
+      {/* <Navbar className="Navbar" varient="light" >
       <div className="container">
           <img src={logo}  
               width="90"
@@ -19,12 +17,9 @@ const MyNav = () => {
               className="d-inline-block align-top" 
               alt="Kidzee_logo" />
         </div>
-        <Container >
-        
-          <Nav className="me-auto Nav">
-          
-            <Nav.Link href="#home">Home</Nav.Link>
-         
+        <Container >        
+          <Nav className="me-auto Nav">          
+            <Nav.Link href="#home">Home</Nav.Link>         
             <Nav.Link href="#contact">Contact</Nav.Link>
             <Nav.Link href="#about">About</Nav.Link>
             <Nav.Link href="#admission">Admission</Nav.Link>
@@ -32,10 +27,58 @@ const MyNav = () => {
             <Nav.Link href="#Login"><i className="bi bi-person-fill"></i></Nav.Link>
           </Nav>
         </Container>
-      </Navbar>
+      </Navbar> */}
 
+      <Navbar
+        collapseOnSelect
+        expand="lg"
+        fixed="bootom"        
+        variant="dark"
+        className="Navbar"
+      >
+        <LinkContainer to="/">
+          <Navbar.Brand href="#home">
+            <img
+              className="logo"
+              src={logo}             
+              alt="KidzeeLogo"
+            />
+          </Navbar.Brand>
+        </LinkContainer>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="w-100 justify-content-end">
+        <LinkContainer to="/">
+              <Nav.Link className="navLink">Home</Nav.Link>
+        </LinkContainer>
+        <LinkContainer to="/about">
+              <Nav.Link className="navLink">About</Nav.Link>
+        </LinkContainer>
+        <LinkContainer to="/contact">
+              <Nav.Link className="navLink">Contact</Nav.Link>
+        </LinkContainer>
+        <LinkContainer to="/admin">
+              <Nav.Link className="navLink">Admin  </Nav.Link>
+        </LinkContainer>
       
-    
+        <LinkContainer to="/signin">
+              <Nav.Link className="navLink">                   
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="18"
+                  height="18"
+                  fill="currentColor"
+                  class="bi bi-person"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z" />
+                </svg>
+              </Nav.Link>
+        </LinkContainer>
+
+        </Nav>
+        </Navbar.Collapse>
+        </Navbar>
     </> 
     );
 }
