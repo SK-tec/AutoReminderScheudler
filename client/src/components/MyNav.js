aca30c9a74f365992dd4e6d0e033403d92450915
SK-tec <samathagamidi@gmail.com>
2023-03-28 18:29:31 +0200

    Code in myNav component cleaned and react-router-dom package added

--- client/src/components/MyNav.js
+++ client/src/components/MyNav.js
@@ -1,35 +1,13 @@
 import logo from "../images/Kidzee_logo.png";
-import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { LinkContainer } from "react-router-bootstrap";
-import NavLink from 'react-router-dom';
+
 
 const MyNav = () => {
     return ( 
     <>
-    
-      {/* <Navbar className="Navbar" varient="light" >
-      <div className="container">
-          <img src={logo}  
-              width="90"
-              height="70"
-              className="d-inline-block align-top" 
-              alt="Kidzee_logo" />
-        </div>
-        <Container >        
-          <Nav className="me-auto Nav">          
-            <Nav.Link href="#home">Home</Nav.Link>         
-            <Nav.Link href="#contact">Contact</Nav.Link>
-            <Nav.Link href="#about">About</Nav.Link>
-            <Nav.Link href="#admission">Admission</Nav.Link>
-            <Nav.Link href="#Administration">Administration</Nav.Link>
-            <Nav.Link href="#Login"><i className="bi bi-person-fill"></i></Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar> */}
-
-      <Navbar
+             <Navbar
         collapseOnSelect
         expand="lg"
         fixed="bootom"        
