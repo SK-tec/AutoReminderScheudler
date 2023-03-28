fcd8da1b267553562928f43bc7711db0ff98f3f2
SK-tec <67866435+SK-tec@users.noreply.github.com>
2023-03-28 17:56:35 +0200

    Update MyNav.js
    import NavLink from 'react-router-dom' is commented out.

--- client/src/components/MyNav.js
+++ client/src/components/MyNav.js
@@ -3,7 +3,7 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { LinkContainer } from "react-router-bootstrap";
-import NavLink from 'react-router-dom';
+// import NavLink from 'react-router-dom';
 
 const MyNav = () => {
     return ( 
