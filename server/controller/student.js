58185a1d09ea3ddb1c06babf7e1fcf79905065ed
samatha <samathagamidi@gmail.com>
2023-03-29 10:17:44 +0200

    controllers are added

---
+++ server/controller/student.js
@@ -0,0 +1,65 @@
+const Student = require('../models/student');
+
+const createStudentProfile = async (req, res) => {
+  try {
+    const newStudent = await Student.create({ ...req.body, createdBy: req.user._id });
+    res.status(201).json(newStudent);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const getAllStudents = async (req, res) => {
+  try {
+    const students = await Student.find().populate('createdBy', 'username email');
+    res.json(students);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+const getStudentById = async (req, res) => {
+  try {
+    // const student = await Student.findById(req.params.id)
+    const students = await Student.find({ _id: req.params.id }).populate('createdBy', 'username email');
+    if (students.length === 0) {
+      res.status(404).json({ message: 'Student Not Found' });
+    }
+    res.json(students[0]);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+const updateStudentProfile= async (req, res) => {
+  try {
+    // const updatedStudent = await Student.findByIdAndUpdate(req.params.id);
+    const updateStudentProfile = await Student.findOneAndUpdate({ _id: req.params.id }, req.body, {
+      new: true,
+    });
+    if (!updateStudentProfile) {
+      res.status(404).json({ message: 'Book Not Found' });
+    }
+    res.json(updateStudentProfile);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+const deleteStudentProfile = async (req, res) => {
+  try {
+    
+    const deletedStudent = await Student.findOneAndDelete({ _id: req.params.id });
+    if (!deletedStudent) {
+      res.status(404).json({ message: 'Student Not Found' });
+    }
+    res.json(deletedStudent);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = {
+  createStudentProfile,
+  getAllStudents,
+  getStudentById,
+  updateStudentProfile,
+  deleteStudentProfile,
+};
