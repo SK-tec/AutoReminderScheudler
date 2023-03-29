ce6b4465204702e8d66f3317fe5157c49d743b37
samatha <samathagamidi@gmail.com>
2023-03-29 14:03:07 +0200

    achivements card made responsive

--- client/src/components/Achivements.js
+++ client/src/components/Achivements.js
@@ -1,37 +1,48 @@
-import {Container,Card, Row, } from 'react-bootstrap';
+import {Container,Card, Row,Col } from 'react-bootstrap';
 import Boy from '../images/boy.jpeg';
 import Girl from '../images/girl.jpeg';
 
 
 const Achivements = () => {
     return (
-        <Container fluid id="achievements">
+        <Container  id="achievements">
             <h1>Achievements</h1>
-            <Row sm={2} lg={4} className='justify-content-center gap-5'>
-            <Card style={{ width: '20rem' }}>
+            <Row xs={2} lg={4} >
+           <Col>
+           <Card  className="mb-2" >
                 <Card.Img  variant="top" src={Boy}  />
                     <Card.Body>
                         <p className='text-center'>Achivement #1</p>
                     </Card.Body>
             </Card>
-            <Card style={{ width: '20rem' }}>
+           </Col>  
+           <Col>
+           <Card  className="mb-2">
                 <Card.Img variant="top" src={Girl} />
                     <Card.Body>
                         <p className='text-center'>Achivement #1</p>
                     </Card.Body>
             </Card>
-            <Card style={{ width: '20rem' }}>
-                <Card.Img variant="top" src={Boy} responsive />
+            </Col> 
+           <Col>
+           <Card  className="mb-2" >
+                <Card.Img variant="top" src={Boy} />
                     <Card.Body>
                         <p className='text-center'>Achivement #1</p>
                     </Card.Body>
             </Card>
-            <Card style={{ width: '20rem' }}>
+            </Col>
+           <Col>
+           <Card  className="mb-2">
                 <Card.Img variant="top" src={Girl} />
                     <Card.Body>
                         <p className='text-center'>Achivement #1</p>
                     </Card.Body>
             </Card>
+            </Col>
+            
+            
+            
             </Row>
         </Container>
     );
