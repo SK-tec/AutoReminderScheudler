299b42dcfd8da1f170fabd4e148cac2dde0780e4
samatha <samathagamidi@gmail.com>
2023-03-30 16:48:16 +0200

    serverroutes are made functional and admin dashboard merged

--- client/src/components/MyNav.js
+++ client/src/components/MyNav.js
@@ -1,66 +1,58 @@
 import logo from "../images/Kidzee_logo.png";
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 import { LinkContainer } from "react-router-bootstrap";
 
-
-
 const MyNav = () => {
-  
-    return ( 
-<>
+  return (
+    <>
       <Navbar
         collapseOnSelect
         expand="lg"
-        fixed="bootom"        
+        fixed="bootom"
         variant="dark"
         className="Navbar"
       >
         <LinkContainer to="/">
           <Navbar.Brand href="#home">
-            <img
-              className="logo"
-              src={logo}             
-              alt="KidzeeLogo"
-            />
+            <img className="logo" src={logo} alt="KidzeeLogo" />
           </Navbar.Brand>
         </LinkContainer>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
-        <Nav className="w-100 justify-content-end">
-        <LinkContainer to="/">
+          <Nav className="w-100 justify-content-end">
+            <LinkContainer to="/">
               <Nav.Link className="navLink">Home</Nav.Link>
-        </LinkContainer>
-        <LinkContainer to="/about">
+            </LinkContainer>
+            <LinkContainer to="/about">
               <Nav.Link className="navLink">About</Nav.Link>
-        </LinkContainer>
-        <LinkContainer to="/contact">
+            </LinkContainer>
+            <LinkContainer to="/contact">
               <Nav.Link className="navLink">Contact</Nav.Link>
-        </LinkContainer>
-        <LinkContainer to="/admin">
-              <Nav.Link className="navLink">Admin  </Nav.Link>
-        </LinkContainer>
-      
-        <LinkContainer to="/signin">
-              <Nav.Link className="navLink">                   
+            </LinkContainer>
+            <LinkContainer to="/admin">
+              <Nav.Link className="navLink">Admin </Nav.Link>
+            </LinkContainer>
+
+            <LinkContainer to="/signin">
+              <Nav.Link className="navLink">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="18"
                   height="18"
                   fill="currentColor"
-                  class="bi bi-person"
+                  className="bi bi-person"
                   viewBox="0 0 16 16"
                 >
                   <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z" />
                 </svg>
               </Nav.Link>
-        </LinkContainer>
-
-        </Nav>
+            </LinkContainer>
+          </Nav>
         </Navbar.Collapse>
-        </Navbar>
-    </> 
-    );
-}
- 
+      </Navbar>
+    </>
+  );
+};
+
 export default MyNav;
