cc51f075bc57867acb13ce918ca8c30ad424f217
Ershia <ershia94@gmail.com>
2023-03-31 11:06:37 +0200

    portnumber

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const  authenticRouter=require('./routes/students');
 
 const path = require("path");
 const cookieParser=require('cookie-parser');
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 const app = express();
 app.use(cors({origin:process.env.FRONTEND_URL, credentials: true }));
 app.use(express.json());
