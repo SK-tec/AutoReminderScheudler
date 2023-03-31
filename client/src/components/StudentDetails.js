37837239807be4ac0686c42425cb7e82a3bd0e5e
Ershia <ershia94@gmail.com>
2023-03-31 13:43:07 +0200

    fetch data studentdetail

--- client/src/components/StudentDetails.js
+++ client/src/components/StudentDetails.js
@@ -1,7 +1,18 @@
+import { useEffect } from "react";
+import axios from '../axiosInstance';
+
 const StudentDetails = () => {
+  useEffect(() => {
+    axios.get("/api/students").then((res) => {
+      console.log(res.data);
+    }).catch(error => {
+      console.log(error.res.data);
+    })
+  });
+
   return (
     <>
-      <h2>This is StudentDetails Page</h2>
+      <h2>Hello </h2>
     </>
   );
 };
