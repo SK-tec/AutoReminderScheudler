de8119ec923a5de799369f572f559d0988a44b2b
samatha <samathagamidi@gmail.com>
2023-04-06 08:40:55 +0200

    Seniors section centered

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -80,9 +80,9 @@ const About = () => {
         </Row>
       </Container>
 
-      <Container className="mt-4 team">
+      <Container className="mt-4 team justify-content-center">
         <h2 className="text-success text-center mb-2">Meet Our Seniors</h2>
-        <div className="row  ">
+        <div className="row justify-content-center ">
           <div className="col-lg-3">
             {/* <svg className="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"/><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg> */}
             <img
