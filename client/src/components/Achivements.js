8c53b6f038a31b7010a2791e88ab939dee865040
samatha <samathagamidi@gmail.com>
2023-04-11 13:56:03 +0200

    images changed

--- client/src/components/Achivements.js
+++ client/src/components/Achivements.js
@@ -1,7 +1,8 @@
 import { Container, Card, Row, Col } from "react-bootstrap";
 import Boy from "../images/boy.jpeg";
 import Girl from "../images/girl.jpeg";
-
+import Achivements3 from "../images/Achivements3.jpg";
+import boy2 from "../images/boy2.png";
 const Achivements = () => {
   return (
     <Container id="achievements" className="mt-5 mb-5">
@@ -25,7 +26,7 @@ const Achivements = () => {
         </Col>
         <Col>
           <Card className="mb-2">
-            <Card.Img variant="top" src={Boy} />
+            <Card.Img variant="top" src={boy2} />
             <Card.Body>
               <p className="text-center">Achivement #1</p>
             </Card.Body>
@@ -33,7 +34,7 @@ const Achivements = () => {
         </Col>
         <Col>
           <Card className="mb-2">
-            <Card.Img variant="top" src={Girl} />
+            <Card.Img variant="top" src={Achivements3} />
             <Card.Body>
               <p className="text-center">Achivement #1</p>
             </Card.Body>
